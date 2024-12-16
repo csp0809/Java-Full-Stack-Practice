4c24dfa86cbcce83fa1ae022712f524a803ddea8
csp0809 <chaitanyapatil1059@gmail.com>
2024-12-16 16:29:56 +0530

    update

--- Java Practice/Wrapper.java
+++ Java Practice/Wrapper.java
@@ -1,12 +1,23 @@
-public class Wrapper{    
-    public static void main(String args[]){    
-    //Converting Integer to int   
+// public class Wrapper{    
+//     public static void main(String args[]){    
+//     //Converting Integer to int   
      
-    Integer a=new Integer(3);    
-    int i=a.intValue();//converting Integer to int  
-    int j=a;//unboxing, now compiler will write a.intValue() internally    
+//     Integer a=new Integer(3);    
+//     int i=a.intValue();//converting Integer to int  
+//     int j=a;//unboxing, now compiler will write a.intValue() internally    
         
-    System.out.println(a+" "+i+" "+j);    
-    }}    
+//     System.out.println(a+" "+i+" "+j);    
+//     }}    
+    
+
+public class Wrapper{  
+    public static void main(String args[]){  
+    //Converting int into Integer  
+    int a=20;  
+    Integer i=Integer.valueOf(a);//converting int into Integer  
+    Integer j=a;//autoboxing, now compiler will write Integer.valueOf(a) internally  
+      
+    System.out.println(a+" "+i+" "+j);  
+    }}  
     
     
