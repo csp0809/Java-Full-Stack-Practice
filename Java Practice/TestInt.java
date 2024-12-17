a03d1ee06f0f396d86fd0b4b229c3bed09991445
csp0809 <chaitanyapatil1059@gmail.com>
2024-12-17 14:45:36 +0530

    updated

--- Java Practice/TestInt.java
+++ Java Practice/TestInt.java
@@ -1,22 +1,45 @@
+// interface I1{
+//     void add();
+// }
+// interface I2{
+//     int area(int x, int y);
+// }
+// class TestInt implements I1,I2{
+//     public void add(){
+//         int x=20,y=10;
+//         System.out.println(x-y);
+//     }
+//     public int area(int x,int y){
+//         int z=x*y;
+//         return z;
+//     }
+//     public static void main(String[] args) {
+//         Pract p=new Pract();
+//         p.add();
+//         int a=p.area(20, 2);
+//         System.out.println(a);
+//     }
+// }
+
 interface I1{
     void add();
 }
 interface I2{
-    int area(int x, int y);
+    int area(int a, int b);
 }
-class TestInt implements I1,I2{
+public class TestInt implements I1,I2{
     public void add(){
-        int x=20,y=10;
-        System.out.println(x-y);
+        int x=10,y=20;
+        System.out.println(x+y);
     }
-    public int area(int x,int y){
+    public int area(int x, int y){
         int z=x*y;
         return z;
     }
     public static void main(String[] args) {
-        Pract p=new Pract();
-        p.add();
-        int a=p.area(20, 2);
+        TestInt oo=new TestInt();
+        oo.add();
+        int a=oo.area(5, 10);
         System.out.println(a);
     }
 }
