48493a6e13e718bb7a9c4d717bd543a8eae62104
csp0809 <chaitanyapatil1059@gmail.com>
2024-12-19 13:37:20 +0530

    update

--- Java Practice/Pr.java
+++ Java Practice/Pr.java
@@ -130,13 +130,13 @@
 // }
 
 
-// public class Pr {
+public class Pr {
 
-//     enum Season{
-//         Summer,Winter,Rainy
-//     }
-//     public static void main(String[] args) {
-//         for(Season s:Season.values())
-//         System.out.println(s);
-//     }
-// }
+    enum Season{
+        Summer,Winter,Rainy
+    }
+    public static void main(String[] args) {
+        for(Season s:Season.values())
+        System.out.println(s);
+    }
+}
