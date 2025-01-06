15a657cdff843a56e56b887df0177a807b84081f
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-06 23:47:57 +0530

    new js

---
+++ Web Development Practice/WD self/p55 js variables data types objects/script.js
@@ -0,0 +1,15 @@
+console.log("Tutorial 55 of wed dev course")
+
+var a=5,b=6;
+
+var c= "Chaitanya";
+var _a="Git";
+// var 31a ="Sonu"; is not allowed i.e variable name starting with number
+
+console.log(a+b+8)
+
+console.log(typeof a, typeof b, typeof c)
+
+const a1=6;
+// const means constant and its value will not change once it is assigned
+// so a1=a1+1 is not allowed
