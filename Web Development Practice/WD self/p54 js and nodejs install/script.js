30890d6e5b1a2fbd613bedf32ccf4c6303f059e9
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-06 16:19:30 +0530

    nodes js start

---
+++ Web Development Practice/WD self/p54 js and nodejs install/script.js
@@ -0,0 +1,18 @@
+alert("Hello World");
+
+console.log("Code is running..")
+
+var a = prompt("Enter Your Number")
+var isTrue = confirm("Are you sure you want to leave this page")
+
+if(isTrue){
+    console.log("Site exited")
+}
+else{
+    console.log("Site not exited")
+}
+console.log("your number is "+a)
+
+document.title = "Hey I am good"
+
+document.body.style.backgroundColor = " red"
