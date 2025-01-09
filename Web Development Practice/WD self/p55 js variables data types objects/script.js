a76189f68d8765740e4518e8e79631d88b31a59e
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-10 00:00:06 +0530

    new

--- Web Development Practice/WD self/p55 js variables data types objects/script.js
+++ Web Development Practice/WD self/p55 js variables data types objects/script.js
@@ -13,3 +13,25 @@ console.log(typeof a, typeof b, typeof c)
 const a1=6;
 // const means constant and its value will not change once it is assigned
 // so a1=a1+1 is not allowed
+
+let x = "Chaitanya Git";
+let y = 62;
+let z = 3.55;
+const p = true;
+let q = undefined;
+let r = null;
+
+console.log(x, y, z, p, q, r)
+console.log(typeof x, typeof y, typeof z, typeof p, typeof q, typeof r)
+
+let o = {
+    "name" : "Chaitanya",
+    "job code": 5600
+    "is_hadsome":true
+}
+
+console.log(o)
+o.salary = "100lakh"
+console.log(o)
+o.salary = "500lakh"
+console.log(o)
