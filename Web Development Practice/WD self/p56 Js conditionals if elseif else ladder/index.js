e62106c3e796f3102d8c249d9abf3bcf0b220ed3
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-11 12:35:31 +0530

    update

--- Web Development Practice/WD self/p56 Js conditionals if elseif else ladder/index.js
+++ Web Development Practice/WD self/p56 Js conditionals if elseif else ladder/index.js
@@ -1,10 +1,31 @@
 console.log("Hello I am conditional tutorial")
 
-let age = 45;
+let age = 3;
+let grace =2;
 
-if(age>18){
+age +=grace
+
+console.log(age)
+console.log(age + grace)
+console.log(age - grace)
+console.log(age / grace)
+console.log(age * grace)
+console.log(age ** grace)
+// ** is power function  
+console.log(age % grace)
+
+if(age >18){
     console.log("You can drive")
 }
 else{
     console.log("You cannot drive")
 }
+
+// === operator compares type of data as well and then gives out result(true/false)
+
+// if(age+grace >18){
+//     console.log("You can drive")
+// }
+// else{
+//     console.log("You cannot drive")
+// }
