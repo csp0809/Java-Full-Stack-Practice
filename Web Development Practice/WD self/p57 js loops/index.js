22b5a7d7a55e562fc18156e148da24ec472a5997
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-12 01:39:16 +0530

    new

---
+++ Web Development Practice/WD self/p57 js loops/index.js
@@ -0,0 +1,38 @@
+console.log("Tutorial on loop")
+
+// let a = 1;
+
+// console.log(a)
+// console.log(a+1)
+
+// for(let i = 0; i < 100; i++){
+//     console.log(a+i);
+// }
+
+// let obj = {
+//     name: "Chaitanya",
+//     role: "Programmer",
+//     company: "NA"
+// }
+
+// for (const key in obj) {
+//     const element = obj[key];
+//     console.log(key, element)
+
+// }
+
+// for (const c of "Chaitanya") {
+//     console.log(c)
+// }
+
+// let i=0;
+// while(i<6){
+//     console.log(i)
+//     i++;
+// }
+
+let i=10;
+do {
+    console.log(i)
+    i++;
+} while (i<6);
