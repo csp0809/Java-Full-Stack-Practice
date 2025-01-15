9ea698251ddcfbd06e8c1e225821f8560f062ed2
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-15 20:25:47 +0530

    new js

---
+++ Web Development Practice/WD self/p67 JavaScript DOM - Children, Parent & Sibling Nodes/script.js
@@ -0,0 +1,9 @@
+console.log("Hello Git")
+
+document.body.firstElementChild
+// first child that is element
+document.body.firstElementChild.childNodes
+// all the child nodes inside text(note: spaces are considere text nodes), element, comment nodes
+document.body.firstElementChild.children
+// only html elements
+
