845455cc1d43813ab7d3985a5c6de7cd2082cb01
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-21 17:37:10 +0530

    update

---
+++ Web Development Practice/WD self/p74 Events, Event Bubbling, setInterval & setTimeout/script.js
@@ -0,0 +1,11 @@
+let btn= document.getElementById("btn")
+// lis of all mouse events
+// https://developer.mozilla.org/en-US/docs/Web/API/Element#mouse_events
+btn.addEventListener("dblclick", ()=>{
+    // alert("I was Clicked. ")
+    document.querySelector(".box").innerHTML= "<b>Yayy you were clicked </b> Enjoy your click!"
+})
+
+btn.addEventListener("contextmenu", ()=>{
+    alert("Dont right click please ")
+})
