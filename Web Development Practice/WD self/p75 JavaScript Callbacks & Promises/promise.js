72f89690419de2068d70fa9935b1d8bcc8858c88
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-28 00:48:03 +0530

    update

--- Web Development Practice/WD self/p75 JavaScript Callbacks & Promises/promise.js
+++ Web Development Practice/WD self/p75 JavaScript Callbacks & Promises/promise.js
@@ -1,1 +1,40 @@
 console.log("This is Promises")
+
+let prom1 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a > 0.5) {
+        reject("No random number was not supporting")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes I am done 2")
+            resolve("Git 2")
+        }, 1000);
+    }
+})
+
+let prom2 = new Promise((resolve, reject) => {
+    let a = Math.random();
+    if (a > 0.5) {
+        reject("No random number was not supporting")
+    }
+    else {
+        setTimeout(() => {
+            console.log("Yes I am done")
+            resolve("Git")
+        }, 3000);
+    }
+})
+
+// prom1.then((a)=>{
+//     console.log(a)
+// }).catch((err)=>{
+//     console.log(err)
+// })
+
+let p3 = Promise.all([prom1, prom2])
+p3.then((a) => {
+    console.log(a)
+}).catch((err) => {
+    console.log(err)
+})
