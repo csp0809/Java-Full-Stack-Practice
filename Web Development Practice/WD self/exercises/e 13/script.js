6bf10c3f950d810e98ec8a364e67756cff06d5ab
csp0809 <chaitanyapatil1059@gmail.com>
2025-01-29 18:25:07 +0530

    update

--- Web Development Practice/WD self/exercises/e 13/script.js
+++ Web Development Practice/WD self/exercises/e 13/script.js
@@ -1,25 +1,26 @@
-function createCard(title, cName, views, monthsOld, duration, thumbnail){
+function createCard(title, cName, views, monthsOld, duration, thumbnail) {
     // Finish this function
     let viewStr
-    if (views <1000000){
-        viewStr =views/1000  + "k";
+    if (views < 1000) {
+        viewStr = views;
     }
-    else if(views >1000000){
-        viewStr =views/1000000  + "M";
+    else if (views > 1000000) {
+        viewStr = views / 1000000 + "M";
     }
-    else{
-        viewStr =views/1000000  + "k";
+    else {
+        viewStr = views / 1000 + "k";
     }
     let html = `<div class="card">
             <div class="image">
-                <img src="${thumbnail}">
+                <img src="${thumbnail}" alt = "">
+                <div class="capsule">${duration}</div>
             </div>
             <div class="text">
                 <h1>${title}</h1>
-                <p>
-                    ${cName} . ${viewStr} views . ${monthsOld} months ago
-                </p>
-            </div>`
+                <p>${cName} . ${viewStr} views . ${monthsOld} months ago </p>
+            </div>
+    </div>`
+    document.querySelector(".container").innerHTML = document.querySelector(".container").innerHTML + html
 }
 
 
