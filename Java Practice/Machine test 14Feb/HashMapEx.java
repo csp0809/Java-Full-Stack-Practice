acc0739b24ee289997ea8f7c8429a0b8ade8b8f5
csp0809 <chaitanyapatil1059@gmail.com>
2025-02-17 00:16:22 +0530

    practice

--- Java Practice/Machine test 14Feb/HashMapEx.java
+++ Java Practice/Machine test 14Feb/HashMapEx.java
@@ -1,13 +1,63 @@
+// import java.util.*;
+
+// public class HashMapEx {
+//     public static void main(String[] args) {
+//         HashMap<Integer, String> h=new HashMap<>();
+//         h.put(100, "Chait1111");
+//         h.put(101, "Chait111122");
+        
+//         for(Map.Entry m:h.entrySet()){
+//             System.out.println(m.getKey()+" "+m.getValue());
+//         }
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class HashMapEx {
+
+//     public static void main(String[] args) {
+//         HashMap<Integer, String> h= new HashMap<>();
+//         h.put(100, "Chait123");
+//         h.put(101, "chait1233");
+
+//         for(Map.Entry m:h.entrySet()){
+//             System.out.println(m.getKey()+" "+m.getValue());
+//         }
+//     }
+// }
+
+
+// import java.util.*;
+
+// public class HashMapEx {
+
+//     public static void main(String[] args) {
+//         HashMap<Integer, String> h= new HashMap<>();
+//         h.put(101,"Chaita");
+//         h.put(111,"Chaita1");
+
+//         for(Map.Entry m:h.entrySet()){
+//             System.out.println(m.getKey()+" "+m.getValue());
+//         }
+//     }
+// }
+
+
+
+
 import java.util.*;
 
 public class HashMapEx {
     public static void main(String[] args) {
         HashMap<Integer, String> h=new HashMap<>();
-        h.put(100, "Chait1111");
-        h.put(101, "Chait111122");
-        
+        h.put(110, "Chaitanya");
+        h.put(120, "Chait");
+
         for(Map.Entry m:h.entrySet()){
-            System.out.println(m.getKey()+" "+m.getValue());
+            System.out.println(m.getKey()+" "+m.getValue());;
         }
+
     }
 }
