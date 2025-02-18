82e78dd120a7cb80ce973028b9e32def3023d98c
csp0809 <chaitanyapatil1059@gmail.com>
2025-02-18 19:09:24 +0530

    update

--- Java Practice/Machine test 14Feb/CustomEx.java
+++ Java Practice/Machine test 14Feb/CustomEx.java
@@ -1,25 +1,57 @@
 
 
-public class CustomEx extends Exception{
+// public class CustomEx extends Exception{
+//     CustomEx(String s){
+//         super(s);
+//     }
+// }
+// class AgeTracker{
+//     public static void ageCheck(int age)throws CustomEx{
+//         if(age>18){
+//             System.out.println("Eligible");
+//         }
+//         else{
+//             throw new CustomEx("Not");
+//         }
+//     }
+//     public static void main(String[] args) {
+//         try {
+//             ageCheck(19);
+//         } catch (Exception e) {
+//             // TODO: handle exception
+//             System.out.println(e);
+//         }
+//     }
+// }
+
+
+// public class CustomEx extends Exception{
+//     CustomEx(String s){
+//         super(s);
+//     }
+// }
+// class AgeTracker{
+//     public static void ageCheck(int age) throws CustomEx{
+//         if(age>18){
+//             System.out.println("Eligible");
+//         }
+//         else{
+//             throw new CustomEx("Not");
+//         }
+//     }
+//     public static void main(String[] args) {
+//         try{
+//             ageCheck(19);
+//         }
+//         catch(Exception e){
+//             System.out.println(e);
+//         }
+//     }
+// }
+
+public class CustomEx extends CustomEx {
     CustomEx(String s){
-        super(s);
+        super(s);           
     }
-}
-class AgeTracker{
-    public static void ageCheck(int age)throws CustomEx{
-        if(age>18){
-            System.out.println("Eligible");
-        }
-        else{
-            throw new CustomEx("Not");
-        }
-    }
-    public static void main(String[] args) {
-        try {
-            ageCheck(19);
-        } catch (Exception e) {
-            // TODO: handle exception
-            System.out.println(e);
-        }
-    }
+    
 }
--- Java Practice/Machine test 14Feb/overrr.java
+++ Java Practice/Machine test 14Feb/overrr.java
@@ -1,11 +1,36 @@
-class Vehicle{
-    void run(){
-      System.out.println("Running....");
-    }
+// class Vehicle{
+//     void run(){
+//       System.out.println("Running....");
+//     }
+//   }
+//   class Bike extends Vehicle{
+//     public static void main(String[] args) {
+//       Bike o=new Bike();
+//       o.run();
+//     }
+//   }
+
+// class Vehicle{
+//   void run(){
+//     System.out.println("Running...");
+//   }
+// }
+// class Bike extends Vehicle{
+//   public static void main(String[] args) {
+//     Bike b=new Bike();
+//     b.run();
+//   }
+// }
+
+class Arith{
+  void add(){
+    int a=32,b=23;
+    System.out.println("Additon is: "+(a+b));
   }
-  class Bike extends Vehicle{
-    public static void main(String[] args) {
-      Bike o=new Bike();
-      o.run();
-    }
+}
+class Calc extends Arith{
+  public static void main(String[] args) {
+    Calc c= new Calc();
+    c.add();
   }
+}
