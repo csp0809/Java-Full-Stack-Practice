83d6f0c8f72e62cbf359977cb6255c3b083ff27e
csp0809 <chaitanyapatil1059@gmail.com>
2025-03-02 18:52:16 +0530

    practice

--- Java Practice/Machine test/prime.java
+++ Java Practice/Machine test/prime.java
@@ -1,3 +1,31 @@
+// import java.util.Scanner;
+
+// public class prime {
+
+//     public static void main(String[] args) {
+//         int no,i,flag=0;
+//         Scanner sc =new Scanner(System.in);
+//         System.out.println("enter no");
+//         no=sc.nextInt();
+
+//         for(i=2;i<no;i++)
+//         {
+//             if(no%i==0)
+//             {
+//                 flag=1;
+//             }
+//         }
+//         if (flag==0) {
+//             System.out.println("Prime");
+//         }
+//         else
+//         {
+//             System.out.println("Not Prime");
+//         }
+
+//     }
+// }
+
 import java.util.Scanner;
 
 public class prime {
