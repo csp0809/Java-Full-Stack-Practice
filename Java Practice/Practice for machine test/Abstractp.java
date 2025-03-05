ddb04f0bfe0fd30f82df889f1282483977f066ea
csp0809 <chaitanyapatil1059@gmail.com>
2025-03-05 14:19:53 +0530

    Update Abstractp.java

--- Java Practice/Practice for machine test/Abstractp.java
+++ Java Practice/Practice for machine test/Abstractp.java
@@ -38,6 +38,44 @@
 // }
 
 
+// //abstract practical
+
+// abstract class shape{
+//     shape(){
+//         System.out.println("constructor called");
+//     }
+//     public void show(){
+//         System.out.println("method called");
+//     }
+//     abstract void area();
+// }
+
+// class Circle extends shape{
+//     void area(){
+//         int r=8;
+//         System.out.println("Area of circle is:"+(3.14*r*r));
+//     }
+// }
+
+// class Rectangle extends shape{
+//     void area(){
+//         int l=5,b=10;
+//         System.out.println("Area of Rectangle:"+(l*b));
+//     }
+// }
+// public class Abstractp {
+//     public static void main(String[] args) {
+//         shape s;
+//         s= new Circle();
+//         s.show();
+//         s.area();
+//         s= new Rectangle();
+//         s.show();
+//         s.area();
+//     }
+    
+// }
+
 //abstract practical
 
 abstract class shape{
