bc8de3e3da4117db9d07255ca12a389377e56a76
csp0809 <chaitanyapatil1059@gmail.com>
2025-03-07 23:52:37 +0530

    Update Statprog.java

--- Java Practice/Practice for machine test/Statprog.java
+++ Java Practice/Practice for machine test/Statprog.java
@@ -1,4 +1,19 @@
 //Static variable and method
+// public class Statprog {
+
+//     static int r=4;
+//     static double a;
+
+//     static void arearofcircle(){
+//         a=3.14*r*r;
+//         System.out.println("Area Of circle is: "+a);
+        
+//     }
+//     public static void main(String args[]){
+//         arearofcircle();
+//     }
+// }
+
 public class Statprog {
 
     static int r=4;
