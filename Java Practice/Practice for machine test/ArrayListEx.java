8c27c3a6025209eb769153e837c5e8914da15df7
csp0809 <chaitanyapatil1059@gmail.com>
2025-03-10 16:46:52 +0530

    Update ArrayListEx.java

--- Java Practice/Practice for machine test/ArrayListEx.java
+++ Java Practice/Practice for machine test/ArrayListEx.java
@@ -35,6 +35,42 @@
 // }
 
 
+// // ArrayList example
+// import java.util.*;
+
+// public class ArrayListEx {
+
+//     int id;
+//     String nm;
+//     int age;
+//     ArrayListEx(int id, String nm, int age){
+//         this.id=id;
+//         this.nm=nm;
+//         this.age=age;
+
+//     }
+// }
+
+// class Test{
+//     public static void main(String[] args) {
+//         ArrayListEx e1=new ArrayListEx(1,"chait" , 26);
+//         ArrayListEx e2=new ArrayListEx(2,"chait2" , 26);
+//         ArrayListEx e3=new ArrayListEx(3,"chait1" , 26);
+
+//         ArrayList<ArrayListEx> al= new ArrayList<ArrayListEx>();
+//         al.add(e1);
+//         al.add(e2);
+//         al.add(e3);
+
+//         Iterator itr=al.iterator();
+
+//         while (itr.hasNext()) {
+//             ArrayListEx s= (ArrayListEx)itr.next();
+//             System.out.println(s.id+" "+s.nm+" "+s.age );
+//         }
+//     }
+// }
+
 // ArrayList example
 import java.util.*;
 
