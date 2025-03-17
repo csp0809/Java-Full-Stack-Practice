764dafb3c56cdb74b8fd317510a7dc318870ec08
csp0809 <chaitanyapatil1059@gmail.com>
2025-03-18 00:06:34 +0530

    update

--- Java Practice/Machine test/rev.java
+++ Java Practice/Machine test/rev.java
@@ -1,3 +1,22 @@
+// import java.util.Scanner;
+
+// public class rev {
+//     public static void main(String[] args) {
+//         int no,rem;
+//         Scanner sc= new Scanner(System.in);
+//         System.out.println("no:");
+//         no = sc.nextInt();
+//         while(no>0)
+//         {
+//             rem=no%10;
+//             System.out.print(rem);
+//             no=no/10;
+//         }
+//     }
+    
+// }
+
+
 import java.util.Scanner;
 
 public class rev {
--- Java Practice/Machine test/strrev.java
+++ Java Practice/Machine test/strrev.java
@@ -1,3 +1,23 @@
+// import java.util.Scanner;
+
+// public class strrev {
+//     public static void main(String[] args) {
+//         String og,rev="";
+
+//         Scanner sc=new Scanner(System.in);
+//         System.out.println("Enter String: ");
+//         og=sc.next();
+
+//         int l=og.length();
+
+//         for(int i=l-1;i>=0;i--)
+//         {
+//             rev=rev+og.charAt(i);
+//         }
+//         System.out.println(rev);
+//     }
+// }
+
 import java.util.Scanner;
 
 public class strrev {
